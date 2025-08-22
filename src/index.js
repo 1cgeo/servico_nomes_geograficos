e2164a87b1fe9dd8f42a0f41fac8ac9e90fed90c
dinizime <diniz.ime@gmail.com>
2025-08-22 14:14:55 -0300

    inicio exportar pdf

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 const fs = require('fs');
 const path = require('path');
 const logFile = path.join(__dirname, 'api-access.log');
+const pdfExportRoutes = require('./routes/pdf-export');
 
 const numCPUs = os.cpus().length;
 const numWorkers = Math.min(Math.floor(numCPUs / 3), 8);
@@ -56,6 +57,7 @@ if (cluster.isMaster) {
     // }
     next();
   });
+  app.use('/', pdfExportRoutes);
 
   app.get('/busca', async (req, res, next) => {
     const { q, lat, lon } = req.query;
